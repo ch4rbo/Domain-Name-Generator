f0e1b7213ec91b12958563c484ff2d8879c7a1ca
ch4rbo <baueratctu@gmail.com>
2021-09-03 18:45:03 +0000

    Corrijo ejercicio, error de sintaxis. Gracias Diego

--- src/app.js
+++ src/app.js
@@ -9,22 +9,20 @@ window.onload = function() {
   let pronouns = ["the", "our"];
   let adjectives = ["great", "big"];
   let nouns = ["jogger", "racoon"];
-  
-  
+
   function dominio() {
-  let domain = [];
-  for (let i = 0; i < pronouns.length; i++) {
-  for (let x = 0; x < adjectives.length; x++) {
-  for (let z = 0; z < nouns.length; z++) {
- let pronoun = pronouns[i];
- let adjective = adjectives[x];
- let noun = nouns[z];
-
- domain.push(pronoun + adjective + noun);
-}
-}
-}
-return domain;
-}
-console
-
+    let domain = [];
+    for (let i = 0; i < pronouns.length; i++) {
+      for (let x = 0; x < adjectives.length; x++) {
+        for (let z = 0; z < nouns.length; z++) {
+          let pronoun = pronouns[i];
+          let adjective = adjectives[x];
+          let noun = nouns[z];
+          domain.push(pronoun + adjective + noun);
+        }
+      }
+    }
+    return domain;
+  }
+  console.log(dominio());
+};
